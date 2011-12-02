1b1c11f273878996cc1ed0c8bdc9d0a43df856ab
Jeremy Kendall <jeremy@jeremykendall.net>
2011-12-02 00:32:23 -0600

    Removes db connection code, reformats code for readability

--- index.php
+++ index.php
@@ -1,18 +1,5 @@
 <?php
-$db = realpath(dirname(__FILE__) . '/data/db/bookshelf.db');
-$dsn = "sqlite:$db";
-$options = array(
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-);
-
-try {
-    $dbh = new PDO($dsn, null, null, $options);
-} catch (PDOException $e) {
-    echo "Error!: " . $e->getMessage() . "<br />\n";
-    die();
-}
-
+require_once dirname(__FILE__) . '/library/base.php';
 $books = $dbh->query("SELECT * FROM bookshelf ORDER BY title")->fetchAll();
 ?>
 
@@ -42,26 +29,26 @@ $books = $dbh->query("SELECT * FROM bookshelf ORDER BY title")->fetchAll();
             <a href="book-form.php">Add Book</a>
         </p>
 
-<?php if (count($books) > 0): ?>
+        <?php if (count($books) > 0): ?>
             <table>
                 <tr>
                     <th>Title</th><th>Author</th>
                 </tr>
-<?php foreach ($books as $book): ?>
-            <tr>
-                <td>
-                    <a href="book-form.php?id=<?php echo $book['id']; ?>">
-<?php echo $book['title']; ?>
+            <?php foreach ($books as $book): ?>
+                <tr>
+                    <td>
+                        <a href="book-form.php?id=<?php echo $book['id']; ?>">
+                        <?php echo $book['title']; ?>
                     </a>
                 </td>
                 <td>
-<?php echo $book['author']; ?>
-                </td>
-            </tr>
-<?php endforeach; ?>
-        </table>
-<?php else: ?>
-        <p>We have no books!</p>
-<?php endif; ?>
+                    <?php echo $book['author']; ?>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+                    </table>
+        <?php else: ?>
+                            <p>We have no books!</p>
+        <?php endif; ?>
     </body>
 </html>
