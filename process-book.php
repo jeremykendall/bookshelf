166eebbeb1cb1432bc35f076ce05c7867085a2a3
Jeremy Kendall <jeremy@jeremykendall.net>
2011-12-02 15:19:48 -0600

    Replaces PDO code with service call

--- process-book.php
+++ process-book.php
@@ -3,24 +3,19 @@
 require_once dirname(__FILE__) . '/library/base.php';
 
 if (strtolower($_SERVER['REQUEST_METHOD']) == 'get') {
-    header("Location: /");
+    header('Location: /');
 }
 
 $id = filter_input(INPUT_POST, 'id', FILTER_VALIDATE_INT);
 $title = filter_input(INPUT_POST, 'title', FILTER_SANITIZE_STRING);
 $author = filter_input(INPUT_POST, 'author', FILTER_SANITIZE_STRING);
 
-if ($id) {
-    $statement = $dbh->prepare("UPDATE bookshelf SET title = :title, author = :author WHERE id = :id");
-    $statement->bindParam(':title', $title);
-    $statement->bindParam(':author', $author);
-    $statement->bindParam(':id', $id);
-    $statement->execute();
-} else {
-    $statement = $dbh->prepare("INSERT INTO bookshelf (title, author) VALUES (:title, :author)");
-    $statement->bindParam(':title', $title);
-    $statement->bindParam(':author', $author);
-    $statement->execute();
-}
+$book = array(
+    'id' => $id,
+    'title' => $title,
+    'author' => $author
+);
 
-header("Location: /");
+$bookshelf->save($book);
+
+header('Location: /');
