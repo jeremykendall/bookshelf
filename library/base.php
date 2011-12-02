e4a33d7d180210f8d4ae2e8a72d83c99465622bd
Jeremy Kendall <jeremy@jeremykendall.net>
2011-12-02 15:14:24 -0600

    Adds code necessary to use new BookshelfService
    Sets include path to include library folder
    Creates simple autoloader for namespaces
    Registers autoloader
    Creates instance of BookshelfService
    Minor code reformattings

--- library/base.php
+++ library/base.php
@@ -5,8 +5,23 @@ error_reporting(-1);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
+set_include_path(
+    implode(PATH_SEPARATOR, array(
+        get_include_path(),
+        dirname(__FILE__)
+        )
+    )
+);
+
+function namespaceAutoloader($class)
+{
+    include str_replace('\\', DIRECTORY_SEPARATOR, $class) . '.php';
+}
+
+spl_autoload_register('namespaceAutoloader');
+
 $db = realpath(dirname(__FILE__) . '/../data/db/bookshelf.db');
-$dsn = "sqlite:$db";
+$dsn = 'sqlite:' . $db;
 $options = array(
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
@@ -16,6 +31,8 @@ try {
     $dbh = new PDO($dsn, null, null, $options);
 } catch (PDOException $e) {
     throw $e;
-    echo "Error!: " . $e->getMessage() . "<br />\n";
+    echo 'Error!: ' . $e->getMessage();
     die();
 }
+
+$bookshelf = new Bookshelf\Service\BookshelfService($dbh);
