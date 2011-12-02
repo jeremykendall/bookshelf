a2c2a5c901a6dddef4509e6fd5619da6ae69b274
Jeremy Kendall <jeremy@jeremykendall.net>
2011-12-02 15:17:38 -0600

    Abstracts business logic and data access

---
+++ library/Bookshelf/Service/BookshelfService.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Bookshelf\Service;
+
+/**
+ * Bookshelf app API
+ *
+ * @category    Bookshelf
+ * @package     Bookshelf\Service
+ */
+
+/**
+ * @category    Bookshelf
+ * @package     Bookshelf\Service
+ */
+class BookshelfService
+{
+
+    /**
+     * @var PDO database handle
+     */
+    private $_dbh;
+
+    /**
+     * Public constructor
+     * 
+     * @param PDO $dbh database handle
+     */
+    public function __construct(\PDO $dbh)
+    {
+        $this->_dbh = $dbh;
+    }
+
+    /**
+     * Find book by its id
+     *
+     * @param  int $id
+     * @return array
+     */
+    public function find($id)
+    {
+        $sql = 'SELECT * FROM bookshelf WHERE id = :id';
+        $statement = $this->_dbh->prepare($sql);
+        $statement->bindParam(':id', $id);
+        $statement->execute();
+        return $statement->fetch();
+    }
+
+    /**
+     * Find all books 
+     * 
+     * @return array
+     */
+    public function findAll()
+    {
+        $sql = 'SELECT * FROM bookshelf ORDER BY title';
+        return $this->_dbh->query($sql)->fetchAll();
+    }
+
+    /**
+     * Saves or updates a book.
+     * 
+     * $options should be an associative array with keys for
+     * id, title, and author
+     * 
+     * @param array $options 
+     */
+    public function save(array $options)
+    {
+        if ($options['id']) {
+            $statement = $this->_dbh->prepare("UPDATE bookshelf SET title = :title, author = :author WHERE id = :id");
+            $statement->execute($options);
+        } else {
+            unset($options['id']);
+            $statement = $this->_dbh->prepare("INSERT INTO bookshelf (title, author) VALUES (:title, :author)");
+            $statement->execute($options);
+        }
+    }
+
+}
