f525f4fbaf91ca9141942ec2ff1c829e7f41252c
Jeremy Kendall <jeremy@jeremykendall.net>
2012-02-21 00:39:33 -0600

    Fixed really ugly code formatting

--- index.php
+++ index.php
@@ -42,26 +42,26 @@ $books = $dbh->query("SELECT * FROM bookshelf ORDER BY title")->fetchAll();
             <a href="book-form.php">Add Book</a>
         </p>
 
-<?php if (count($books) > 0): ?>
+        <?php if (count($books) > 0): ?>
             <table>
                 <tr>
                     <th>Title</th><th>Author</th>
                 </tr>
-<?php foreach ($books as $book): ?>
-            <tr>
-                <td>
-                    <a href="book-form.php?id=<?php echo $book['id']; ?>">
-<?php echo $book['title']; ?>
-                    </a>
-                </td>
-                <td>
-<?php echo $book['author']; ?>
-                </td>
-            </tr>
-<?php endforeach; ?>
-        </table>
-<?php else: ?>
-        <p>We have no books!</p>
-<?php endif; ?>
+                <?php foreach ($books as $book): ?>
+                    <tr>
+                        <td>
+                            <a href="book-form.php?id=<?php echo $book['id']; ?>">
+                                <?php echo $book['title']; ?>
+                            </a>
+                        </td>
+                        <td>
+                            <?php echo $book['author']; ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </table>
+        <?php else: ?>
+            <p>We have no books!</p>
+        <?php endif; ?>
     </body>
 </html>
