e746c1c753bb5cbb541e380c7821f45d9ba3e844
Jeremy Kendall <jeremy@jeremykendall.net>
2013-05-16 11:44:09 -0500

    Implements delete

--- delete-book.php
+++ delete-book.php
@@ -4,7 +4,7 @@ $db = mysql_connect('localhost', 'testuser', 'testpass');
 mysql_select_db('bookshelf', $db);
 
 if ($_GET['id']) {
-    mysql_query("INSERT INTO bookshelf (title, author) VALUES ('{$_POST['title']}', '{$_POST['author']}')");
+    mysql_query("DELETE FROM bookshelf WHERE id = {$_GET['id']}");
 }
 
 header("Location: /");
